a3fd060907726753bc4984f77560f233f4d0a762
Sanchali Palai <palaisanchali@gmail.com>
2026-07-21 18:40:13 +0530

    Fix Whisper transcription on Railway

--- transcribe.py
+++ transcribe.py
@@ -1,18 +1,26 @@
 import whisper
-import os
 
 
-# Tell Python where FFmpeg is
-os.environ["PATH"] += os.pathsep + r"C:\Users\spala\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.1.2-full_build\bin"
-
+print("Loading Whisper model...")
 
 model = whisper.load_model("base")
 
+print("Whisper model loaded")
+
 
 def transcribe_video(video_path):
+
+    print("Transcribing:", video_path)
 
     result = model.transcribe(
         video_path
     )
 
-    return result["text"]
+    transcript = result["text"]
+
+    print(
+        "Transcript:",
+        transcript
+    )
+
+    return transcript
